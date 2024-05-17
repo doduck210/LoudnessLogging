4fee5f1779cfce90d745869b3d35bdf7771aefb1
Ducky <duck@sbs.co.kr>
2024-05-17 10:20:11 +0900

    fixed untracted file

--- getLoudness.py
+++ getLoudness.py
@@ -1,16 +1,17 @@
-import pyloudnorm as pyln
+import lkfs
 import soundfile as sf
 import subprocess
 import os
 
 def getLoudness(file):
     data, rate = sf.read(file) #data.shape : (길이, 채널수) 
-    meter = pyln.Meter(rate)
+    meter = lkfs.Meter(rate)
     loudness = meter.integrated_loudness(data)
     return loudness
 
 def splitAndLoud(file_path,start_time,duration):
-    subprocess.run(['ffmpeg','-i',file_path,'-ss',str(start_time),'-t',str(duration),'-c','copy',"./data/tmpWav.wav"],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    lufs = getLoudness("./data/tmpWav.wav")
-    os.remove("./data/tmpWav.wav")
+    fileName="./data/"+str(start_time)+"tmpWav.wav"
+    #subprocess.run(['ffmpeg','-i',file_path,'-ss',str(start_time),'-t',str(duration),'-c','copy',"./data/"+str(start_time)+"tmpWav.wav"],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    lufs = getLoudness(fileName)
+    #os.remove("./data/"+str(start_time)+"tmpWav.wav")
     return lufs
