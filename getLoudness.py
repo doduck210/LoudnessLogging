3f164d79d53358dbfaae4802d727e55eb36e536c
Ducky <duck@sbs.co.kr>
2024-08-02 10:55:40 +0900

    recording test

--- getLoudness.py
+++ getLoudness.py
@@ -15,3 +15,11 @@ def splitAndLoud(file_path,start_time,duration):
     lufs, mlkfs = getLoudness(fileName)
     os.remove(fileName)
     return lufs, mlkfs
+
+if __name__ == "__main__":
+    file = 'recording/output_105137.wav'
+    data, rate = sf.read(file=file)
+    data=data[:,0:2]
+    meter=lkfs.Meter(rate)
+    loudness,mlkfs = meter.integrated_loudness(data)
+    print(data.shape)
